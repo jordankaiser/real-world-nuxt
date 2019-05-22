9c9ee6c3b1edc29b30af8b7a730c0350c92e5104
Jordan Kaiser <jordan.ross.kaiser@gmail.com>
2019-05-22 17:10:08 -0500

    snapshot of tutorial progress

---
+++ pages/create.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+<script>
+export default {
+  // head() is used by vue-meta which is included with nuxt.
+  head() {
+    return {
+      title: 'Create title',
+      meta: [
+        {
+          hid: 'hid description',
+          name: 'name description',
+          content:
+            'Donec nec nisi vitae felis facilisis dictum at vulputate sapien. Maecenas elementum arcu at leo sagittis, sed ultrices massa maximus.'
+        }
+      ]
+    }
+  }
+}
+</script>
