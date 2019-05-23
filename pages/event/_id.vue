903a3fb0c618e5c742b171e5bc1d6bb5cd7590d7
Jordan Kaiser <jordan.ross.kaiser@gmail.com>
2019-05-23 10:24:26 -0500

    Snapshot

---
+++ pages/event/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Event #{{ id }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  // head() is used by vue-meta which is included with nuxt.
+  head() {
+    return {
+      title: 'Event #' + this.id,
+      meta: [
+        {
+          hid: 'hid description',
+          name: 'name description',
+          content: 'What you need to know about event #' + this.id
+        }
+      ]
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
